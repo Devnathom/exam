1497430ca4b577846f8efb84327f5e4632877441
Ratchadet Srikaeo <ratchadetsrikaeo@192.168.1.25>
2026-02-20 07:44:25 +0700

    fix: add webpack alias for @/* paths in next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': require('path').resolve(__dirname, 'src'),
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
